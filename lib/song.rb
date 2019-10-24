9cf671bb2e7f5f532a0dce6204e18589175c9927
Rachel T <rachmaybe@gmail.com>
2019-10-24 05:57:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,8 @@ require 'pry'
 class Song
   attr_accessor :name
   attr_reader :artist
-
+  extend Memorable
+  
   @@songs = []
 
   def initialize
@@ -16,14 +17,6 @@ class Song
 
   def self.all
     @@songs
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def artist=(artist)
